c27cc31888e8d89a783a3cf08fb3f0b303ca21c3
Ben LaForge <benlaforge@gmail.com>
2017-06-24 23:03:55 -0400

    took away unnecessary sections

---
+++ js/ben_js.js
@@ -0,0 +1,10 @@
+$(window).load(function(){
+  $(window).scroll(function() {
+    var wintop = $(window).scrollTop(), docheight = $('body').height(), winheight = $(window).height();
+    console.log(wintop);
+    var totalScroll = (wintop/(docheight-winheight))*100;
+    console.log("total scroll" + totalScroll);
+    $(".progressBar").css("width",totalScroll+"%");
+  });
+
+});
