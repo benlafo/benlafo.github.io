1bb96513bc4567d1f00460fec82fd6bdb130b18e
Ben LaForge <benlaforge@gmail.com>
2017-09-05 12:55:10 -0400

    added slideshows and other things

--- js/ben_js.js
+++ js/ben_js.js
@@ -8,3 +8,22 @@ $(window).load(function(){
   });
 
 });
+
+var slideIndex = 1;
+showDivs(slideIndex, "myYolkSlides");
+showDivs(slideIndex, "factbookSlides");
+
+function plusDivs(n,set) {
+    showDivs(slideIndex += n, set);
+}
+
+function showDivs(n, set) {
+    var i;
+    var x = document.getElementsByClassName(set);
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} 
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
+}
